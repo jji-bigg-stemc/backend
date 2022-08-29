304c1ddf859c57c106ec94a9bf48077a50f7cb4d
Jerry Ji <jji.bigg@gmail.com>
2022-08-28 17:40:36 -0700

    adding code info consistency

---
+++ models/sheets/users.js
@@ -0,0 +1,1 @@
+// defining the sheets model in storing the users data in the spreadsheet from env vars
--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,20 @@ const { google } = require("googleapis");
 
 const { authSheets } = require("../funcs/googleapis");
 
+// update these below to match dev and prod environments
+// const SHEET_NAME = "Users";
+const SHEET_NAME = "Sheet1";
+
+const CRED_FILE = "credentials-dev.json";
+
+const SHEET_ID =
+  process.env.SHEET_ID || "1IfBG0PgM3v4pf5UBcqwQo3EogqsieCdNG1BLEz9zEFg";
+
+// a pet function; please ignore this
 router.get("/", async (req, res) => {
-  const spreadsheetId =
-    process.env.spreadsheetId || "1IfBG0PgM3v4pf5UBcqwQo3EogqsieCdNG1BLEz9zEFg";
-  const { auth, authClient, sheets } = await authSheets("credentials-dev.json");
+  const { auth, authClient, sheets } = await authSheets(CRED_FILE);
   const getRows = await sheets.spreadsheets.values.get({
-    spreadsheetId,
+    SHEET_ID,
     range: "Sheet1!A1:E",
   });
   res.send(getRows.data);
@@ -19,17 +27,18 @@ router.get("/", async (req, res) => {
 router.post("/login", async (req, res) => {});
 
 router.post("/register", async (req, res) => {
+  // maybe use this after registering from the frontend: Auth0
+  // then just update infos here for record keeping
+
   // perform data cleaning and validation for the posted data
   const newUser = req.body;
 
   // create a new row in the spreadsheet
-  const spreadsheetId =
-    process.env.spreadsheetId || "1IfBG0PgM3v4pf5UBcqwQo3EogqsieCdNG1BLEz9zEFg";
 
-  const { auth, authClient, sheets } = await authSheets("credentials-dev.json");
+  const { auth, authClient, sheets } = await authSheets(CRED_FILE);
   const newRow = await sheets.spreadsheets.values.append({
-    spreadsheetId,
-    range: "Sheet1",
+    SHEET_ID,
+    range: SHEET_NAME,
     valueInputOption: "USER_ENTERED",
     resource: {
       values: [newUser],
@@ -39,16 +48,15 @@ router.post("/register", async (req, res) => {
 
 router.put("/update", async (req, res) => {
   // perform data cleaning and validation for the posted data
+  // know what field is updated and do the updates in iterative fashion
   const updatedUser = req.body;
 
   // update a row in the spreadsheet
-  const spreadsheetId =
-    process.env.spreadsheetId || "1IfBG0PgM3v4pf5UBcqwQo3EogqsieCdNG1BLEz9zEFg";
 
-  const { auth, authClient, sheets } = await authSheets("credentials-dev.json");
+  const { auth, authClient, sheets } = await authSheets(CRED_FILE);
   const newRow = await sheets.spreadsheets.values.update({
-    spreadsheetId: "1IfBG0PgM3v4pf5UBcqwQo3EogqsieCdNG1BLEz9zEFg",
-    range: "Sheet1!E2",
+    SHEET_ID: "1IfBG0PgM3v4pf5UBcqwQo3EogqsieCdNG1BLEz9zEFg",
+    range: `${SHEET_NAME}!E2`,
     valueInputOption: "USER_ENTERED",
     resource: {
       values: [updatedUser],
